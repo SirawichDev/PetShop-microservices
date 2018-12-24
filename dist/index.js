45b733a4cf7f3f8b5e47108aec0b6f2ac6b4bd35
SirawichDev <sirawit0676@gmail.com>
2018-12-24 19:02:54 +0700

    fixed forget export

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const AppConfig_1 = require("./config/AppConfig");
-exports.default = new AppConfig_1.default();
-//# sourceMappingURL=index.js.map
