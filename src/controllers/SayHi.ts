215b12b7cec4a785c77d9711ba9a768b0a851713
SirawichDev <sirawit0676@gmail.com>
2018-12-24 18:53:10 +0700

    add controller get

---
+++ src/controllers/SayHi.ts
@@ -0,0 +1,11 @@
+import { Controller, Get, JsonController, Post } from 'routing-controllers';
+
+@Controller('/miewmiew')
+class SayHi {
+    @Get('/v1')
+    async get(): Promise<any> {
+        return {
+            'sirawich': 'SaWaddee Krabbbb :3'
+        };
+    }
+}
