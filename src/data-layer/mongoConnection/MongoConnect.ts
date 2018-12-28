a6e24f7eb3c956e40c8ffe5f6743f2a3119a3193
SirawichDev <sirawit0676@gmail.com>
2018-12-28 16:50:12 +0700

    connect to mongo

---
+++ src/data-layer/mongoConnection/MongoConnect.ts
@@ -0,0 +1,26 @@
+import Mongo = require('mongoose');
+import * as config from 'config';
+
+Mongo.Promise = global.Promise;
+
+class MongoConnect {
+    static mongooseInstance: any;
+    static mongooseConnection: Mongo.Connection;
+
+    constructor() {
+        MongoConnect.letsConnect();
+    }
+    static letsConnect(): Mongo.Connection {
+        if (this.mongooseInstance) {
+            return this.mongooseInstance;
+        }
+        let connectionString = config.get('mongo.urlClient').toString();
+        this.mongooseConnection = Mongo.connection;
+
+        this.mongooseConnection.once('open', () => {
+            console.info('Connected to mongo');
+        });
+
+        this.mongooseInstance = Mongo.connect(connectionString);
+    }
+}
